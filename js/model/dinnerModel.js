e191d86725c79875612190eb6089fc73fdddb388
conradhi <35565897+conradhi@users.noreply.github.com>
2018-02-14 21:39:25 +0100

    lab 2

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -1,6 +1,5 @@
 //DinnerModel Object constructor
 var DinnerModel = function() {
-	//TODO Lab 1 implement the data structure that will hold number of guest
 	// and selected dishes for the dinner menu
 	var numberOfGuests = 1;
 	var menu = []; //this stores only id's of dishes in our current menu
